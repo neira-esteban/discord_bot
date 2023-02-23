d701bd7a25347bb7d2b73faf9b66ff0d9dc433b6
Esteban Neira <neira.e99@gmail.com>
2023-02-23 19:08:45 -0300

    cambios SFW

--- commands/play.js
+++ commands/play.js
@@ -5,7 +5,7 @@ module.exports = {
   run: async (client, message, args) => {
     const string = args.join(" ");
     if (!string)
-      return message.channel.send(`Ingresa el nombre de una canción, wn pao`);
+      return message.channel.send(`Estimado, ingrese el nombre de una canción`);
     client.DisTube.play(message.member.voice.channel, string, {
       member: message.member,
       textChannel: message.channel,
